81562ed23c2789ed1480f62ce0e6955860a6fab8
Kamun Rustem <r.kamun@gmail.com>
2017-05-19 16:52:29 +0600

    task for yernar

--- celery_uncovered/toyex/tasks.py
+++ celery_uncovered/toyex/tasks.py
@@ -14,6 +14,8 @@ def send_test_email_task():
 
 
 r"""
+Responsible: Sattar Stamkukov <devishot>
+
 
 Create a task that will fetch hottest repositories from github per day, week, month (say 500), group them by language and put the result to the csv file
 
@@ -42,4 +44,38 @@ Required Libraries:
     requests
     django.core.mail
 """
+
+
+r"""
+Responsible: Mailubayev Yernar <mailubai@gmail.com>
+
+Create a logging handler that will be able to track Server errors (50X) and report them to admins through via celery. I advice to thoroughly understand https://docs.djangoproject.com/en/1.11/howto/error-reporting/
+
+Seems like you need to extend default  'django.utils.log.AdminEmailHandler',
+
+
+Implementation Details:
+
+1. Create self containable task called `report_error_task/4 similar to `self.send_mail(subject, message, fail_silently=True, html_message=html_message)``
+
+2. Extend 'django.utils.log.AdminEmailHandler' in the way it will call report_error_task.delay with the required parameters
+
+3. Return nothing (Ensure that ignore_result flag set to True)
+
+4. Provide http tests that ensures that it is called
+
+Bonus tasks for readers:
+
+1. Modify code so that task could be scheduled once per time range (1 hour, 6 hours) and all bugs will be collected and send as one email, rather than notifying often.
+Hint: you also need to create models.py that will store the HttpErrorEntries
+
+
+Required Libraries:
+    django.core.mail
+    django.util.log
+    mailhog
+    pytest
+"""
+
+
 
