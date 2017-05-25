a2f59c63c8a287540badb9845be9ef84aed1a5f1
Kamun Rustem <r.kamun@gmail.com>
2017-05-26 00:10:35 +0600

    signatures allowed

--- celery_uncovered/tricks/celery_ext.py
+++ celery_uncovered/tricks/celery_ext.py
@@ -45,6 +45,54 @@ class ScopeBasedTask(current_app.Task):
         task_rv = super(ScopeBasedTask, self).__call__(*args, **kwargs)
         return task_rv
 
+    def signature(self, args=None, *starargs, **starkwargs):
+        """
+        def sig(args=None, *starargs, **starkwargs):
+
+            print args
+            print starargs
+            print starkwargs
+
+        sig(args=1, *(1,2,3), **{}) - violates
+        sig(args=1, *(1,2,3), **{'args': (1, 2, 3)}) - violates
+        sig(args=1, *(), **{}) - possible
+        sig(1, *(1, 2, 3), **{}) - possible
+
+        .sig
+            signature(args, kwargs)
+            signature(args=None, kwargs=None)
+        """
+        # WARN reconcialiate args, kwargs with celery
+        if len(starargs) == 0:
+            starargs = args
+            kwargs = starkwargs.setdefault('kwargs', {})
+            self.set_scenario(scenario_id=kwargs.get('scenario_id', None))
+            self.inject_scope_args(kwargs)
+            return super(ScopeBasedTask, self).signature(args=args, **starkwargs)
+        else:
+            self.set_scenario(scenario_id=starargs[0].get('scenario_id', None))
+            self.inject_scope_args(starargs[0])
+            # starargs[0].update(starkwargs)
+            self.log_msg("SIGNATURE called with args=%s, starargs=%s, starkwargs=%s", list(args), list(starargs), starkwargs)
+            return super(ScopeBasedTask, self).signature(args, *starargs, **starkwargs)
+
+    subtask = signature
+
+    def s(self, *a, **kw):
+        """Create signature.
+
+        Shortcut for ``.s(*a, **k) -> .signature(a, k)``.
+        """
+        return self.signature(a, kw)
+
+    def si(self, *a, **kw):
+        """Create immutable signature.
+
+        Shortcut for ``.si(*a, **k) -> .signature(a, k, immutable=True)``.
+        """
+        return self.signature(a, kw, immutable=True)
+
+
     def inject_scope_args(self, kwargs):
         for arg in self.scope_args:
             if arg not in kwargs:
