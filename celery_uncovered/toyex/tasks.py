a542ef140b9eca3812499de6b9fc05d8c67c472d
Kamun Rustem <r.kamun@gmail.com>
2017-06-21 10:04:13 +0600

    polishing github task

--- celery_uncovered/toyex/tasks.py
+++ celery_uncovered/toyex/tasks.py
@@ -4,7 +4,7 @@ import requests
 from celery import shared_task, group
 from django.core.mail import mail_admins
 from django.conf import settings
-from .utils import make_csv
+from .utils import make_csv, strf_date
 from .models import Repository
 import datetime
 
@@ -64,34 +64,19 @@ def fetch_hot_repos(since, per_page, page):
     items = r.json()[u'items']
     return [Repository(item) for item in items]
 
+
 @shared_task
-def produce_hot_repo_report_task(ref_date):
+def produce_hot_repo_report_task(period, ref_date=None):
     # 1. parse date
-    str_date = None
-    if ref_date in ('day', 'week', 'month'):
-        now = datetime.date.today()
-        delta = None
-        if ref_date is 'day':
-            delta = datetime.timedelta(days=1)
-        elif ref_date is 'week':
-            delta = datetime.timedelta(weeks=1)
-        else:
-            delta = datetime.timedelta(days=30)
-        str_date = (now - delta).isoformat()
-
-    elif type(ref_date) is str:
-        str_date = ref_date
-
-    elif type(ref_date) is datetime.date:
-        str_date = ref_date.isoformat()
+    ref_date_str = strf_date(period, ref_date=ref_date)
 
     # 2. fetch and join
     job = group([
-        fetch_hot_repos.s(str_date, 100, 1),
-        fetch_hot_repos.s(str_date, 100, 2),
-        fetch_hot_repos.s(str_date, 100, 3),
-        fetch_hot_repos.s(str_date, 100, 4),
-        fetch_hot_repos.s(str_date, 100, 5)
+        fetch_hot_repos.s(ref_date_str, 100, 1),
+        fetch_hot_repos.s(ref_date_str, 100, 2),
+        fetch_hot_repos.s(ref_date_str, 100, 3),
+        fetch_hot_repos.s(ref_date_str, 100, 4),
+        fetch_hot_repos.s(ref_date_str, 100, 5)
     ])
     result = job.apply_async()
     all_repos = []
@@ -111,7 +96,7 @@ def produce_hot_repo_report_task(ref_date):
     for lang in sorted(grouped_repos.keys()):
         lines.append([lang] + grouped_repos[lang])
 
-    filename = '{media}/github-hot-repos-{date}.csv'.format(media=settings.MEDIA_ROOT, date=str_date)
+    filename = '{media}/github-hot-repos-{date}.csv'.format(media=settings.MEDIA_ROOT, date=ref_date_str)
     return make_csv(filename, lines)
 
 
--- celery_uncovered/toyex/utils.py
+++ celery_uncovered/toyex/utils.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import datetime
+
 
 def make_csv(filename, lines):
     dirname = os.path.dirname(filename)
@@ -10,3 +12,26 @@ def make_csv(filename, lines):
         for line in lines:
             trending_csv.writerow(line)
     return filename
+
+
+
+def strf_date(mixed_date, ref_date=None):
+    dt_str = None
+    if ref_date is None:
+        ref_date = datetime.date.today()
+    if mixed_date in ('day', 'week', 'month'):
+        delta = None
+        if mixed_date is 'day':
+            delta = datetime.timedelta(days=1)
+        elif mixed_date is 'week':
+            delta = datetime.timedelta(weeks=1)
+        else:
+            delta = datetime.timedelta(days=30)
+        dt_str = (ref_date - delta).isoformat()
+
+    elif type(ref_date) in (str, unicode):
+        dt_str = ref_date
+
+    elif type(ref_date) in (datetime.date, datetime.datetime):
+        dt_str = ref_date.isoformat()
+    return dt_str
